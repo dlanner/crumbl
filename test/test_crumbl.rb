87fde97e9b45324cc85309e9b4bb8efa49b4ef01
David Lanner <david.lanner@pgi.com>
2014-10-07 22:50:03 -0400

    add tests for custom iterations

--- test/test_crumbl.rb
+++ test/test_crumbl.rb
@@ -9,4 +9,23 @@ class CrumblTest < Minitest::Test
     decrypted = Crumbl.decrypt(encrypted, key_base)
     assert_equal data, decrypted
   end
+
+  def test_custom_iterations_equal
+    data = {:foo => "bar"}
+    key_base = "secret key base"
+    iterations = 1001
+    encrypted = Crumbl.encrypt(data, key_base, iterations)
+    decrypted = Crumbl.decrypt(encrypted, key_base, iterations)
+    assert_equal data, decrypted
+  end
+
+  def test_custom_iterations_not_equal
+    data = {:foo => "bar"}
+    key_base = "secret key base"
+    iterations = 1001
+    encrypted = Crumbl.encrypt(data, key_base)
+    assert_raises ActiveSupport::MessageVerifier::InvalidSignature do
+      Crumbl.decrypt(encrypted, key_base, iterations)
+    end
+  end
 end
