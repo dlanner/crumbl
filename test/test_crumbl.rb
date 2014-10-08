963b0adf3cdeee2a0e5de0689750ca36a80a82eb
David Lanner <david.lanner@pgi.com>
2014-10-07 22:53:49 -0400

    add tests for custom salts

--- test/test_crumbl.rb
+++ test/test_crumbl.rb
@@ -22,10 +22,30 @@ class CrumblTest < Minitest::Test
   def test_custom_iterations_not_equal
     data = {:foo => "bar"}
     key_base = "secret key base"
-    iterations = 1001
+    custom_iterations = 1001
     encrypted = Crumbl.encrypt(data, key_base)
     assert_raises ActiveSupport::MessageVerifier::InvalidSignature do
-      Crumbl.decrypt(encrypted, key_base, iterations)
+      Crumbl.decrypt(encrypted, key_base, custom_iterations)
+    end
+  end
+
+  def test_custom_encrypted_cookie_salt
+    data = {:foo => "bar"}
+    key_base = "secret key base"
+    custom_encrypted_cookie_salt = "salty"
+    encrypted = Crumbl.encrypt(data, key_base)
+    assert_raises ActiveSupport::MessageEncryptor::InvalidMessage do
+      Crumbl.decrypt(encrypted, key_base, Crumbl::DEFAULT_ITERATIONS, custom_encrypted_cookie_salt)
+    end
+  end
+
+  def test_custom_encrypted_signed_cookie_salt
+    data = {:foo => "bar"}
+    key_base = "secret key base"
+    custom_encrypted_signed_cookie_salt = "salty"
+    encrypted = Crumbl.encrypt(data, key_base)
+    assert_raises ActiveSupport::MessageEncryptor::InvalidMessage do
+      Crumbl.decrypt(encrypted, key_base, Crumbl::DEFAULT_ITERATIONS, custom_encrypted_signed_cookie_salt)
     end
   end
 end
