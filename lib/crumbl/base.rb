7ed27cb10b053974dab3fa47d4835e26e8a2380b
David Lanner <david.lanner@pgi.com>
2014-12-26 12:58:58 -0500

    allow JSON input; DRY up message encryptor building code

--- lib/crumbl/base.rb
+++ lib/crumbl/base.rb
@@ -26,18 +26,28 @@ class Crumbl
   end
 
   def decrypt cookie, secret_key_base
-    key_generator = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: options[:iterations])
-    secret = key_generator.generate_key(options[:encrypted_cookie_salt])
-    sign_secret = key_generator.generate_key(options[:encrypted_signed_cookie_salt])
-    encryptor = ActiveSupport::MessageEncryptor.new(secret, sign_secret)
+    encryptor = build_message_encryptor(secret_key_base)
     encryptor.decrypt_and_verify(cookie)
   end
 
   def encrypt data, secret_key_base
-    key_generator = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: options[:iterations])
-    secret = key_generator.generate_key(options[:encrypted_cookie_salt])
-    sign_secret = key_generator.generate_key(options[:encrypted_signed_cookie_salt])
-    encryptor = ActiveSupport::MessageEncryptor.new(secret, sign_secret)
+    encryptor = build_message_encryptor(secret_key_base)
     encryptor.encrypt_and_sign(data)
+  end
+
+  private
+
+  def build_message_encryptor secret_key_base
+    key_generator = ActiveSupport::KeyGenerator.new(secret_key_base, iterations: options[:iterations])
+    secret        = key_generator.generate_key(options[:encrypted_cookie_salt])
+    sign_secret   = key_generator.generate_key(options[:encrypted_signed_cookie_salt])
+    ActiveSupport::MessageEncryptor.new(secret, sign_secret, serializer: ActiveSupport::MessageEncryptor::NullSerializer)
+  end
+
+end
+
+class String
+  def is_json?
+    !!JSON.parse(self) rescue false
   end
 end
